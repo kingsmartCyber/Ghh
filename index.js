a8c29fc7a1afabdb12a3f50df1903e91f183c0b9
Kingsmart Cyber <162454101+kingsmartCyber@users.noreply.github.com>
2025-08-01 13:30:23 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express= require('express');
+const app = express();
+
+const PORT = 1000
+
+app.get('/', (req, res) => {
+  res.sendFile('index.html')
+});
+
+app.listen(PORT, () => {
+  console.log(`App running 💨`);
+});
